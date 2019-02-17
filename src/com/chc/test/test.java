fd0f162f62f991086b25295dd60ff84feb177690
Renjith Rohan Alanjikkal Ravindran <renjithrohanar@gmail.com>
2019-02-17 16:45:20 -0600

    This is the first JAVA project

---
+++ src/com/chc/test/test.java
@@ -0,0 +1,13 @@
+package com.chc.test;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is the first Java Project in this laptop");
+		
+		
+	}
+
+}
